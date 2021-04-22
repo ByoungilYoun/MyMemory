f9816a679756bdf4af077f0aaabd9685cd5e764b
YounByoungil <jimmy7525@naver.com>
2021-04-23 00:37:37 +0900

    ListViewController tableview cellForRowAt 구현

--- MyMemory/MyMemory/Controller/ListViewController.swift
+++ MyMemory/MyMemory/Controller/ListViewController.swift
@@ -12,6 +12,9 @@ class ListViewController : UIViewController {
   //MARK: - Properties
   private let tableView = UITableView()
   
+  // 앱 델리게이트 객체의 참조 정보를 가져온다.
+  let appDelegate = UIApplication.shared.delegate as! AppDelegate
+  
   //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -19,6 +22,10 @@ class ListViewController : UIViewController {
     configureUI()
   }
   
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.tableView.reloadData()
+  }
   //MARK: - Functions
   private func setNavi() {
     navigationItem.title = "목록"
@@ -51,17 +58,36 @@ class ListViewController : UIViewController {
   //MARK: - UITableViewDataSource
 extension ListViewController : UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 2
+    let count = self.appDelegate.memoList.count
+    return count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    if indexPath.row == 0 {
-      let cell = tableView.dequeueReusableCell(withIdentifier: MemoCell.identifier, for: indexPath) as! MemoCell
-      cell.selectionStyle = .none
+    
+    // memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼낸다.
+    let row = self.appDelegate.memoList[indexPath.row]
+    
+    if row.image == nil {
+      let cell = tableView.dequeueReusableCell(withIdentifier: MemoCell.identifier) as! MemoCell
+      cell.titleLabel.text = row.title
+      cell.subTitleLabel.text = row.contents
+    
+      let formatter = DateFormatter()
+      formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+      cell.timeLabel.text = formatter.string(from: row.regdate!)
+      
       return cell
+
     } else {
-      let cell = tableView.dequeueReusableCell(withIdentifier: MemoCellWithImage.identifier, for: indexPath) as! MemoCellWithImage
-      cell.selectionStyle = .none
+      let cell = tableView.dequeueReusableCell(withIdentifier: MemoCellWithImage.identifier) as! MemoCellWithImage
+      cell.titleLabel.text = row.title
+      cell.subTitleLabel.text = row.contents
+      cell.myImageView.image = row.image
+      
+      let formatter = DateFormatter()
+      formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+      cell.timeLabel.text = formatter.string(from: row.regdate!)
+      
       return cell
     }
   }
@@ -69,9 +95,6 @@ extension ListViewController : UITableViewDataSource {
   //MARK: - UITableViewDelegate
 extension ListViewController : UITableViewDelegate {
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//    print("123")
-//    let cell = tableView.cellForRow(at: indexPath) as! MemoCell
-//
     let controller = MemoPreviewController()
     navigationController?.pushViewController(controller, animated: true)
   }
